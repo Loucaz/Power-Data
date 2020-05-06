b198121b59caefe848e8cadf8555d4b6c2112075
julesperret <perretjules@yahoo.fr>
2020-05-06 15:03:30 +0200

    creation page accueil

--- vue-first-application/src/views/Home.vue
+++ vue-first-application/src/views/Home.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App">
-      <p>Salut</p>
-    </HelloWorld>
+  <div class="container home">
+    <h1 class="bienvenu">Bienvenu sur Power Data</h1>
+    <div class="row button-home">
+      <div class="col"></div>
+      <div class="col"> <a href="#"><button class="inscrire">S'inscrire</button></a></div>
+      <div class="col"><a href="#ancresavoirplus"> <button class="ensavoirplus">En savoir plus</button></a></div>
+       <div class="col"></div>
+    </div>
+    <h2 id="ancresavoirplus">Que-est-ce que PowerData ?</h2>
+    <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita repellendus optio laboriosam blanditiis nobis, nemo, earum totam deleniti ducimus quia hic saepe ipsam omnis aspernatur iste libero, doloribus id quis sunt. Eius odio, nam maiores amet, eos quis totam a sunt quidem aperiam iste eveniet perspiciatis tenetur repellendus rerum modi. Sit molestiae delectus dolor velit! Molestias dolores a corporis placeat consectetur ad ipsum accusamus exercitationem officiis magni eum, minima ipsa voluptates qui dolorem quidem illum, eos, culpa sequi mollitia. A sit, aut commodi soluta dolores ab dignissimos! Nostrum velit possimus illum praesentium. At a maiores qui laborum earum libero accusamus.</p>
+    <h2>Comment l'utiliser ?</h2>
+    <p>Mettre la video de presentation</p>
+    <p>+ liens es tutoriels</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-};
+
 </script>
