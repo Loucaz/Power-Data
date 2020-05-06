c708652ca31af1ae21ae1525f0e30ea9b3193c7d
julesperret <perretjules@yahoo.fr>
2020-05-06 16:31:32 +0200

    page d'accueil

--- vue-first-application/src/views/Home.vue
+++ vue-first-application/src/views/Home.vue
@@ -10,8 +10,51 @@
     <h2 id="ancresavoirplus">Que-est-ce que PowerData ?</h2>
     <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita repellendus optio laboriosam blanditiis nobis, nemo, earum totam deleniti ducimus quia hic saepe ipsam omnis aspernatur iste libero, doloribus id quis sunt. Eius odio, nam maiores amet, eos quis totam a sunt quidem aperiam iste eveniet perspiciatis tenetur repellendus rerum modi. Sit molestiae delectus dolor velit! Molestias dolores a corporis placeat consectetur ad ipsum accusamus exercitationem officiis magni eum, minima ipsa voluptates qui dolorem quidem illum, eos, culpa sequi mollitia. A sit, aut commodi soluta dolores ab dignissimos! Nostrum velit possimus illum praesentium. At a maiores qui laborum earum libero accusamus.</p>
     <h2>Comment l'utiliser ?</h2>
-    <p>Mettre la video de presentation</p>
-    <p>+ liens es tutoriels</p>
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col video"><p>Mettre la video de presentation</p></div>
+      <div class="col-3"></div>
+    </div>
+    <h3>Apprenez grace aux tutoriels </h3>
+    <div class="row lignetuto">
+      <div class="col"></div>
+     <div class="col-2 tuto">
+          <router-link to="/">
+            <div class="table-icon table-blue">
+            <i class="fa fa-database" aria-hidden="true"></i>
+          </div>
+          <p>Tuto : <br/> Créer une base</p>
+          </router-link>
+      </div>
+      <div class="col"></div>
+      <div class="col-2 tuto">
+        <router-link to="/posts">
+            <div class="table-icon table-orange">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+          </div>
+          <p>Tuto : <br/> Créer des tables</p>
+        </router-link>
+      </div>
+      <div class="col"></div>
+      <div class="col-2 tuto">
+          <router-link to="/">
+            <div class="table-icon table-green">
+            <i class="fa fa-check-square-o" aria-hidden="true"></i>
+          </div>
+          <p>Tuto : <br/> Créer des formulaires</p>
+          </router-link>
+      </div>
+      <div class="col"></div>
+     <div class="col-2 tuto">
+          <router-link to="/">
+            <div class="table-icon table-red">
+            <i class="fa fa-flag-checkered" aria-hidden="true"></i>
+          </div>
+          <p>Tuto : <br/> Collecte données</p>
+          </router-link>
+      </div>
+      <div class="col"></div>
+    </div>
   </div>
 </template>
 
