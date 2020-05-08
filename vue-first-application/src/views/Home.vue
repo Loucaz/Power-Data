ce64195a9bb27b242f204fa3268f91a753baa1a8
julesperret <perretjules@yahoo.fr>
2020-05-08 10:50:43 +0200

    home et tuto sans texte

--- vue-first-application/src/views/Home.vue
+++ vue-first-application/src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
+<div class="container-fluid">
   <div class="container home">
-    <h1 class="bienvenu">Bienvenu sur Power Data</h1>
+    <h1 class="bienvenu">Bienvenue sur Power Data</h1>
     <div class="row button-home">
       <div class="col"></div>
       <div class="col"> <a href="#"><button class="inscrire">S'inscrire</button></a></div>
       <div class="col"><a href="#ancresavoirplus"> <button class="ensavoirplus">En savoir plus</button></a></div>
        <div class="col"></div>
     </div>
+  </div>
     <h2 id="ancresavoirplus">Que-est-ce que PowerData ?</h2>
     <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita repellendus optio laboriosam blanditiis nobis, nemo, earum totam deleniti ducimus quia hic saepe ipsam omnis aspernatur iste libero, doloribus id quis sunt. Eius odio, nam maiores amet, eos quis totam a sunt quidem aperiam iste eveniet perspiciatis tenetur repellendus rerum modi. Sit molestiae delectus dolor velit! Molestias dolores a corporis placeat consectetur ad ipsum accusamus exercitationem officiis magni eum, minima ipsa voluptates qui dolorem quidem illum, eos, culpa sequi mollitia. A sit, aut commodi soluta dolores ab dignissimos! Nostrum velit possimus illum praesentium. At a maiores qui laborum earum libero accusamus.</p>
     <h2>Comment l'utiliser ?</h2>
@@ -55,7 +57,7 @@
       </div>
       <div class="col"></div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
